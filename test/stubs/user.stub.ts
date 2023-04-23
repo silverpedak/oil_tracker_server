12ae892b2345023e4c555f4f1b8dc9609007d08f
“silverpedak” <silver.pedak@gmail.com>
2023-04-23 10:53:21 +0300

    [Tests]: AuthService unit tests

--- test/stubs/user.stub.ts
+++ test/stubs/user.stub.ts
@@ -1,7 +1,9 @@
+import mongoose, { Types } from 'mongoose';
 import { User } from 'src/users';
 
 export const userStub = (): User => {
   return new User({
+    _id: new mongoose.Schema.Types.ObjectId('mongoId'),
     username: 'username',
     password: 'password',
     firstName: 'firstName',
